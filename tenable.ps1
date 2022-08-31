028a78bce17c4e82c27ef5601b46e9f7b334bc9d
ken188 <liukm188@126.com>
2022-08-31 13:40:29 +0800

    Add files via upload

---
+++ tenable.ps1
@@ -0,0 +1,1 @@
+msiexec /i c:\NessusAgent-10.2.0-x64.msi NESSUS_GROUPS="WindowsServer" NESSUS_SERVER="172.20.1.104:443" NESSUS_KEY=48ef1fb2860f663d3d62ce83dfca8a56891a16aba913136e8d8ef47d8cdadc12 /qn
